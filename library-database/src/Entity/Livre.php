f57fc0dde73ef568ad153426d2a53f7bc2551ff7
SacleuxBenoit <benoitsacleux@outlook.fr>
2022-08-21 22:12:03 +0200

    create entity for Livre

---
+++ library-database/src/Entity/Livre.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LivreRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LivreRepository::class)]
+class Livre
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 190)]
+    private ?string $titre = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $annee_edition = null;
+
+    #[ORM\Column]
+    private ?int $nombre_pages = null;
+
+    #[ORM\Column(length: 190, nullable: true)]
+    private ?string $code_isbn = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getAnneeEdition(): ?int
+    {
+        return $this->annee_edition;
+    }
+
+    public function setAnneeEdition(?int $annee_edition): self
+    {
+        $this->annee_edition = $annee_edition;
+
+        return $this;
+    }
+
+    public function getNombrePages(): ?int
+    {
+        return $this->nombre_pages;
+    }
+
+    public function setNombrePages(int $nombre_pages): self
+    {
+        $this->nombre_pages = $nombre_pages;
+
+        return $this;
+    }
+
+    public function getCodeIsbn(): ?string
+    {
+        return $this->code_isbn;
+    }
+
+    public function setCodeIsbn(?string $code_isbn): self
+    {
+        $this->code_isbn = $code_isbn;
+
+        return $this;
+    }
+}
