fed3c648d6ed349a5c4f93ea30ae4adba3e2a159
SacleuxBenoit <benoitsacleux@outlook.fr>
2022-09-14 15:46:46 +0200

    :memo: correction of misspelling for setRoles

--- library-database/src/DataFixtures/TestFixtures.php
+++ library-database/src/DataFixtures/TestFixtures.php
@@ -73,7 +73,7 @@ class TestFixtures extends Fixture
         for($i=0;$i<100;$i++){
             $user = new User();
             $user->setEmail($faker->email());
-            $user->setRoles("[ROLES_EMPRUNTEUR]");
+            $user->setRoles("[ROLE_EMPRUNTEUR]");
             $user->setPassword("$2y$10$/H2ChUxriH.0Q33g3EUEx.S2s4j/rGJH2G88jK9nCP60GbUW8mi5K");
             $user->setEnabled($faker->boolean());
             // date for created_at
