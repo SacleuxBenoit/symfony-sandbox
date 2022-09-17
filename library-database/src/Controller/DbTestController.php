f1d8774422c5243b6588f8a6303eb56284f0813a
SacleuxBenoit <benoitsacleux@outlook.fr>
2022-09-17 17:48:15 +0200

    display the entiere list of users

--- library-database/src/Controller/DbTestController.php
+++ library-database/src/Controller/DbTestController.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\User;
+
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,6 +11,9 @@ class DbTestController extends AbstractController{
     #[Route('/db/test',name:"app_db_test")]
     public function index(ManagerRegistry $doctrine) :Response
     {
+        $repository = $doctrine->getRepository(User::class);
+        $user = $repository->findAll();
+        dump($user);
         exit();
     }
 }
