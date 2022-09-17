04d58298ea637571cfe51902bd088c59099467c9
SacleuxBenoit <benoitsacleux@outlook.fr>
2022-09-17 21:17:34 +0200

    display every datas in controller DbTestController

--- library-database/src/Controller/DbTestController.php
+++ library-database/src/Controller/DbTestController.php
@@ -2,6 +2,11 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Entity\Livre;
+use App\Entity\Emprunteur;
+use App\Entity\Emprunt;
+use App\Entity\Genre;
+use App\Entity\Auteur;
 
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,9 +16,35 @@ class DbTestController extends AbstractController{
     #[Route('/db/test',name:"app_db_test")]
     public function index(ManagerRegistry $doctrine) :Response
     {
+        // User
         $repository = $doctrine->getRepository(User::class);
         $user = $repository->findAll();
         dump($user);
+
+        // Book
+        $repository = $doctrine->getRepository(Livre::class);
+        $livre = $repository->findAll();
+        dump($livre);
+
+        // Emprunteur
+        $repository = $doctrine->getRepository(Emprunteur::class);
+        $emprunteur = $repository->findAll();
+        dump($emprunteur);
+
+        // Emprunt
+        $repository = $doctrine->getRepository(Emprunt::class);
+        $emprunt = $repository->findAll();
+        dump($emprunt);
+
+        // Genre
+        $repository = $doctrine->getRepository(Genre::class);
+        $genre = $repository->findAll();
+        dump($genre);
+
+        // Auteur
+        $repository = $doctrine->getRepository(Auteur::class);
+        $auteur = $repository->findAll();
+        dump($auteur);
         exit();
     }
 }
